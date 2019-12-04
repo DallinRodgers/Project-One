529575babec178bbe11c8e80db9f385b82a306aa
DallinRodgers <rodgers.dallin@gmail.com>
2019-12-04 09:50:01 -0700

    Update maps.js
    Commented maps.js

--- groupProject1/assets/js/maps.js
+++ groupProject1/assets/js/maps.js
@@ -1,71 +1,71 @@
-var map;
-let latResult = 40.7608;
-let lngResult = -111.891;
+// var map;
+// let latResult = 40.7608;
+// let lngResult = -111.891;
 
-function initMap() {
-  // New Map
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: latResult, lng: lngResult },
-    zoom: 14
-  });
+// function initMap() {
+//   // New Map
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: latResult, lng: lngResult },
+//     zoom: 14
+//   });
 
-  //   Add Marker Function
-  function addMarker(location) {
-    var marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-  }
+//   //   Add Marker Function
+//   function addMarker(location) {
+//     var marker = new google.maps.Marker({
+//       position: location,
+//       map: map
+//     });
+//   }
 
-  // Add markers
-  for (var i = 0; i < markerArray.length; i++) {
-    addMarker(markerArray[i]);
-  }
-}
+//   // Add markers
+//   for (var i = 0; i < markerArray.length; i++) {
+//     addMarker(markerArray[i]);
+//   }
+// }
 
-// Array of marker locations
-// Locations will come from yelp results
-var markerArray = [
-  { lat: 40.7608, lng: -111.891 },
-  { lat: 40.7614, lng: -111.888 },
-  { lat: 40.76, lng: -111.893 }
-];
+// // Array of marker locations
+// // Locations will come from yelp results
+// var markerArray = [
+//   { lat: 40.7608, lng: -111.891 },
+//   { lat: 40.7614, lng: -111.888 },
+//   { lat: 40.76, lng: -111.893 }
+// ];
 
-// How to add new locations to markerArray
-// markerArray.push({ lat: latResult, lng: lngResult });
+// // How to add new locations to markerArray
+// // markerArray.push({ lat: latResult, lng: lngResult });
 
-document.querySelector("button").addEventListener("click", () => {
-  let city = document.querySelector("#location").value;
-  let state = document.querySelector("#state").value;
+// document.querySelector("button").addEventListener("click", () => {
+//   let city = document.querySelector("#location").value;
+//   let state = document.querySelector("#state").value;
 
-  //   Remove blank spaces from string and replace with "+"
-  //   This is needed for geoCode
-  city = city.split("");
-  for (let i = 0; i < city.length; i++) {
-    if (city[i] === " ") {
-      city[i] = "+";
-    }
-  }
-  city = city.join("");
+//   //   Remove blank spaces from string and replace with "+"
+//   //   This is needed for geoCode
+//   city = city.split("");
+//   for (let i = 0; i < city.length; i++) {
+//     if (city[i] === " ") {
+//       city[i] = "+";
+//     }
+//   }
+//   city = city.join("");
 
-  geoCode(city, state);
-});
+//   geoCode(city, state);
+// });
 
-// This will get the latitude and longitude of the entered city and state
-// Then call initMap for the new location
-function geoCode(city, state) {
-  fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state}&key=AIzaSyAQniP-3rD977wqZuDvbfDQjBeUNRlXKDo`
-  )
-    .then(result => {
-      return result.json();
-    })
-    .then(data => {
-      // Get longitude and latitude for global variables
-      latResult = data.results[0].geometry.location.lat;
-      lngResult = data.results[0].geometry.location.lng;
-      // Display new location on map
-      initMap();
-    })
-    .catch(error => console.log(error));
-}
+// // This will get the latitude and longitude of the entered city and state
+// // Then call initMap for the new location
+// function geoCode(city, state) {
+//   fetch(
+//     `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state}&key=AIzaSyAQniP-3rD977wqZuDvbfDQjBeUNRlXKDo`
+//   )
+//     .then(result => {
+//       return result.json();
+//     })
+//     .then(data => {
+//       // Get longitude and latitude for global variables
+//       latResult = data.results[0].geometry.location.lat;
+//       lngResult = data.results[0].geometry.location.lng;
+//       // Display new location on map
+//       initMap();
+//     })
+//     .catch(error => console.log(error));
+// }
